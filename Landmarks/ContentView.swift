39447602078b22b13aef25aee0d370820aeb9ffe
Casey Key <casey.key@protonmail.com>
2020-06-09 08:35:53 -0600

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Key, Casey on 6/9/20.
+//  Copyright © 2020 Key, Casey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
